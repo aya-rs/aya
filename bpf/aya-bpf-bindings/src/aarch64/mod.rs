713494609e7314a0cb9bf1c54cb0a5955793c2ee
Alessandro Decina <alessandro.d@gmail.com>
2021-02-24 03:15:56 +0000

    aya-bpf-bindings: remove unused code

--- bpf/aya-bpf-bindings/src/aarch64/mod.rs
+++ bpf/aya-bpf-bindings/src/aarch64/mod.rs
@@ -1,21 +1,3 @@
 pub mod bindings;
 pub mod getters;
 pub mod helpers;
-
-use aya_bpf_cty::{c_long, c_void};
-use core::mem::{self, MaybeUninit};
-
-#[inline]
-unsafe fn bpf_probe_read<T>(src: *const T) -> Result<T, c_long> {
-    let mut v: MaybeUninit<T> = MaybeUninit::uninit();
-    let ret = helpers::bpf_probe_read(
-        v.as_mut_ptr() as *mut c_void,
-        mem::size_of::<T>() as u32,
-        src as *const c_void,
-    );
-    if ret < 0 {
-        return Err(ret);
-    }
-
-    Ok(v.assume_init())
-}
