3fddc8165c6e2c28c4fda19e72156b640da4a0c7
Alessandro Decina <alessandro.d@gmail.com>
2021-03-06 09:48:22 +0000

    aya: make ProgramArray::new private

--- aya/src/maps/program_array.rs
+++ aya/src/maps/program_array.rs
@@ -17,7 +17,7 @@ pub struct ProgramArray<T: Deref<Target = Map>> {
 }
 
 impl<T: Deref<Target = Map>> ProgramArray<T> {
-    pub fn new(map: T) -> Result<ProgramArray<T>, MapError> {
+    fn new(map: T) -> Result<ProgramArray<T>, MapError> {
         let map_type = map.obj.def.map_type;
         if map_type != BPF_MAP_TYPE_PROG_ARRAY as u32 {
             return Err(MapError::InvalidMapType {
