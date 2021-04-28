9595bd45718b19de9a472fbc652f3e4c49f9a1b0
Alessandro Decina <alessandro.d@gmail.com>
2021-04-28 08:53:05 +0000

    aya-gen: don't generate doc comments

--- aya-gen/src/bindgen.rs
+++ aya-gen/src/bindgen.rs
@@ -3,6 +3,7 @@ use bindgen::{self, Builder, EnumVariation};
 pub fn user_builder() -> Builder {
     let bindgen = bindgen::builder()
         .layout_tests(false)
+        .generate_comments(false)
         .prepend_enum_name(false)
         .default_enum_style(EnumVariation::Rust {
             non_exhaustive: false,
@@ -16,6 +17,7 @@ pub fn bpf_builder() -> Builder {
         .use_core()
         .ctypes_prefix("::aya_bpf_cty")
         .layout_tests(false)
+        .generate_comments(false)
         .clang_arg("-Wno-unknown-attributes")
         .default_enum_style(EnumVariation::ModuleConsts)
         .prepend_enum_name(false);
