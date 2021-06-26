f6e3cbbed4458cc5407a51fc3b97e95c0ba7da90
Arnabjyoti Kalita <arnabjyotikalita35@gmail.com>
2021-06-26 15:53:48 +0530

    aya: add minimum kernel version for each map and program tyoes

--- aya/src/maps/stack_trace.rs
+++ aya/src/maps/stack_trace.rs
@@ -17,6 +17,10 @@ use crate::{
 /// useful for profiling, to associate a trace to an event, etc. You can capture traces calling
 /// `stack_id = bpf_get_stackid(ctx, map, flags)` from eBPF, and then you can retrieve the traces
 /// from their stack ids.
+///
+/// # Minimum kernel version
+///
+/// The minimum kernel version required to use this feature is 4.6
 ///  
 /// # Examples
 ///
