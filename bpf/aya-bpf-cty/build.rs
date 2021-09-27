076e2d03048e915f2dc1c19f00f798b1b696b95a
Dave Tucker <dave@dtucker.co.uk>
2021-09-27 13:35:47 +0100

    bpf: Fix clippy warnings
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- bpf/aya-bpf-cty/build.rs
+++ bpf/aya-bpf-cty/build.rs
@@ -3,7 +3,7 @@ use std::env;
 fn main() {
     if env::var("CARGO_CFG_BPF_TARGET_ARCH").is_err() {
         let arch = env::var("HOST").unwrap();
-        let arch = arch.splitn(2, '-').next().unwrap();
+        let arch = arch.split_once('-').map_or(&*arch, |x| x.0);
         println!("cargo:rustc-cfg=bpf_target_arch=\"{}\"", arch);
     }
 }
