49f6a8e81949e127559d7f5698abe19fca4be853
Dan Everton <deverton@expedia.com>
2021-11-16 16:02:19 +1000

    Stub `kernel_version` for tests

--- aya/src/sys/mod.rs
+++ aya/src/sys/mod.rs
@@ -75,6 +75,12 @@ unsafe fn syscall_impl(call: Syscall) -> SysResult {
     Ok(ret)
 }
 
+#[cfg(test)]
+pub(crate) fn kernel_version() -> Result<(u32, u32, u32), ()> {
+    return Ok((0xff, 0xff, 0xff))
+}
+
+#[cfg(not(test))]
 pub(crate) fn kernel_version() -> Result<(u32, u32, u32), ()> {
     unsafe {
         let mut v = mem::zeroed::<utsname>();
