1d06b41e578f41aff8ded77d53a8d1dfb7e250ea
Dave Tucker <dave@dtucker.co.uk>
2022-06-07 15:41:15 +0100

    ci: Test all architectures
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- bpf/aya-bpf-bindings/build.rs
+++ bpf/aya-bpf-bindings/build.rs
@@ -1,7 +1,9 @@
 use std::env;
 
 fn main() {
-    if env::var("CARGO_CFG_BPF_TARGET_ARCH").is_err() {
+    if let Ok(arch) = env::var("CARGO_CFG_BPF_TARGET_ARCH") {
+        println!("cargo:rustc-cfg=bpf_target_arch=\"{}\"", arch);
+    } else {
         let arch = env::var("HOST").unwrap();
         let arch = arch.split_once('-').map_or(&*arch, |x| x.0);
         println!("cargo:rustc-cfg=bpf_target_arch=\"{}\"", arch);
