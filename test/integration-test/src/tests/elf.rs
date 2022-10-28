be61ad7243fe988d0825dec56e85628b430e82c7
Dmitry Savintsev <dsavints@gmail.com>
2022-10-28 14:07:52 +0000

    Make the integration tests handle errors internally.
    Instead of returning anyhow>>Result<()> handle errors
    'in-place' with unwrap or panic, for more informative
    and user-friendly error messages on test failures.
    
    Fixes #421.
    
    Signed-off-by: Dmitry Savintsev <dsavints@gmail.com>

--- test/integration-test/src/tests/elf.rs
+++ test/integration-test/src/tests/elf.rs
@@ -1,15 +1,14 @@
 use super::{integration_test, IntegrationTest};
 
-use anyhow::bail;
 use aya::include_bytes_aligned;
 use object::{Object, ObjectSymbol};
 
 #[integration_test]
-fn test_maps() -> anyhow::Result<()> {
+fn test_maps() {
     let bytes = include_bytes_aligned!("../../../../target/bpfel-unknown-none/debug/map_test");
-    let obj_file = object::File::parse(bytes)?;
+    let obj_file = object::File::parse(bytes).unwrap();
     if obj_file.section_by_name("maps").is_none() {
-        bail!("No 'maps' ELF section");
+        panic!("No 'maps' ELF section");
     }
     let mut found = false;
     for sym in obj_file.symbols() {
@@ -21,7 +20,6 @@ fn test_maps() -> anyhow::Result<()> {
         }
     }
     if !found {
-        bail!("No symbol 'BAR' in ELF file")
+        panic!("No symbol 'BAR' in ELF file")
     }
-    Ok(())
 }
--- test/integration-test/src/tests/smoke.rs
+++ test/integration-test/src/tests/smoke.rs
@@ -8,29 +8,28 @@ use log::info;
 use super::{integration_test, kernel_version, IntegrationTest};
 
 #[integration_test]
-fn xdp() -> anyhow::Result<()> {
+fn xdp() {
     let bytes = include_bytes_aligned!("../../../../target/bpfel-unknown-none/debug/pass");
-    let mut bpf = Bpf::load(bytes)?;
+    let mut bpf = Bpf::load(bytes).unwrap();
     let dispatcher: &mut Xdp = bpf.program_mut("pass").unwrap().try_into().unwrap();
     dispatcher.load().unwrap();
     dispatcher.attach("lo", XdpFlags::default()).unwrap();
-    Ok(())
 }
 
 #[integration_test]
-fn extension() -> anyhow::Result<()> {
-    let (major, minor, _) = kernel_version()?;
+fn extension() {
+    let (major, minor, _) = kernel_version().unwrap();
     if major < 5 || minor < 9 {
         info!(
             "skipping as {}.{} does not meet version requirement of 5.9",
             major, minor
         );
-        return Ok(());
+        return;
     }
     // TODO: Check kernel version == 5.9 or later
     let main_bytes =
         include_bytes_aligned!("../../../../target/bpfel-unknown-none/debug/main.bpf.o");
-    let mut bpf = Bpf::load(main_bytes)?;
+    let mut bpf = Bpf::load(main_bytes).unwrap();
     let pass: &mut Xdp = bpf.program_mut("pass").unwrap().try_into().unwrap();
     pass.load().unwrap();
     pass.attach("lo", XdpFlags::default()).unwrap();
@@ -39,5 +38,4 @@ fn extension() -> anyhow::Result<()> {
     let mut bpf = BpfLoader::new().extension("drop").load(ext_bytes).unwrap();
     let drop_: &mut Extension = bpf.program_mut("drop").unwrap().try_into().unwrap();
     drop_.load(pass.fd().unwrap(), "xdp_pass").unwrap();
-    Ok(())
 }
