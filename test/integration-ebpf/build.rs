a84bd1a95dab2f0092d0ce2d67f508477199c0bc
Tamir Duberstein <tamird@gmail.com>
2023-07-20 10:51:34 -0400

    test: document cargo build scripts

--- test/integration-ebpf/build.rs
+++ test/integration-ebpf/build.rs
@@ -2,6 +2,18 @@ use std::{env, path::PathBuf};
 
 use xtask::{create_symlink_to_binary, AYA_BUILD_INTEGRATION_BPF};
 
+/// Building this crate has an undeclared dependency on the `bpf-linker` binary. This would be
+/// better expressed by [artifact-dependencies][bindeps] but issues such as
+/// https://github.com/rust-lang/cargo/issues/12385 make their use impractical for the time being.
+///
+/// This file implements an imperfect solution: it causes cargo to rebuild the crate whenever the
+/// mtime of `which bpf-linker` changes. Note that possibility that a new bpf-linker is added to
+/// $PATH ahead of the one used as the cache key still exists. Solving this in the general case
+/// would require rebuild-if-changed-env=PATH *and* rebuild-if-changed={every-directory-in-PATH}
+/// which would likely mean far too much cache invalidation.
+///
+/// [bindeps]: https://doc.rust-lang.org/nightly/cargo/reference/unstable.html?highlight=feature#artifact-dependencies
+
 fn main() {
     println!("cargo:rerun-if-env-changed={}", AYA_BUILD_INTEGRATION_BPF);
 
