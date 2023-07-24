4b892e1a4b966e785dba2e144dab6046682f607a
Andrew Stoycos <astoycos@redhat.com>
2023-07-24 12:16:23 -0400

    add methods to help the loaded_programs() API
    Add helper methods to get useful information from the ProgramInfo
    object which is returned by the `loaded_programs()` API.  Specifically
    this code mirrors the bpftool program in terms of useful fields.
    
    The methods here are not exhaustive and may expand in the future.
    
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- aya/src/programs/utils.rs
+++ aya/src/programs/utils.rs
@@ -1,5 +1,13 @@
 //! Common functions shared between multiple eBPF program types.
-use std::{ffi::CStr, io, os::unix::io::RawFd, path::Path};
+use std::{
+    ffi::CStr,
+    fs::File,
+    io,
+    io::{BufRead, BufReader},
+    os::unix::io::RawFd,
+    path::Path,
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
 use crate::{
     programs::{FdLink, Link, ProgramData, ProgramError},
@@ -23,7 +31,7 @@ pub(crate) fn attach_raw_tracepoint<T: Link + From<FdLink>>(
     program_data.links.insert(FdLink::new(pfd).into())
 }
 
-/// Find tracefs filesystem path
+/// Find tracefs filesystem path.
 pub(crate) fn find_tracefs_path() -> Result<&'static Path, ProgramError> {
     lazy_static::lazy_static! {
         static ref TRACE_FS: Option<&'static Path> = {
@@ -50,4 +58,45 @@ pub(crate) fn find_tracefs_path() -> Result<&'static Path, ProgramError> {
     TRACE_FS
         .as_deref()
         .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "tracefs not found").into())
+}
+
+/// Get the real (wall-clock) boot time.
+pub(crate) fn boot_time() -> SystemTime {
+    let mut time = unsafe { std::mem::zeroed::<libc::timespec>() };
+
+    let ret = unsafe { libc::clock_gettime(libc::CLOCK_BOOTTIME, &mut time) };
+    assert_eq!(ret, 0, "failed to get system bootime");
+    let libc::timespec { tv_sec, tv_nsec } = time;
+
+    let dur_since_boot = Duration::new(tv_sec as u64, tv_nsec as u32);
+
+    let ret = unsafe { libc::clock_gettime(libc::CLOCK_REALTIME, &mut time) };
+    assert_eq!(ret, 0, "failed to get system realtime");
+    let libc::timespec { tv_sec, tv_nsec } = time;
+
+    let dur_since_epoch = Duration::new(tv_sec as u64, tv_nsec as u32);
+
+    UNIX_EPOCH + (dur_since_epoch - dur_since_boot)
+}
+
+/// Get the specified information from a file descriptor's fdinfo.
+pub(crate) fn get_fdinfo(fd: RawFd, key: &str) -> Result<u32, ProgramError> {
+    let info = File::open(format!("/proc/self/fdinfo/{}", fd))?;
+    let reader = BufReader::new(info);
+    for line in reader.lines() {
+        match line {
+            Ok(l) => {
+                if !l.contains(key) {
+                    continue;
+                }
+
+                let (_key, val) = l.rsplit_once('\t').unwrap();
+
+                return Ok(val.parse().unwrap());
+            }
+            Err(e) => return Err(ProgramError::IOError(e)),
+        }
+    }
+
+    Ok(0)
 }
