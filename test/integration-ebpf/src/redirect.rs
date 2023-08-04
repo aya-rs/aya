6b58b147dff18726f0e055a2f0bddcd52aa967e8
Tuetuopay <tuetuopay@me.com>
2023-08-05 00:16:10 +0200

    xdp: add support for chained xdp programs in {cpu,dev}map
    set/insert functions can now take an optional bpf program fd to run once
    the packet has been redirected from the main probe

--- test/integration-ebpf/src/redirect.rs
+++ test/integration-ebpf/src/redirect.rs
@@ -4,7 +4,7 @@
 use aya_bpf::{
     bindings::xdp_action,
     macros::{map, xdp},
-    maps::{CpuMap, DevMap, DevMapHash, XskMap},
+    maps::{Array, CpuMap, DevMap, DevMapHash, XskMap},
     programs::XdpContext,
 };
 
@@ -17,6 +17,13 @@ static DEVS_HASH: DevMapHash = DevMapHash::with_max_entries(1, 0);
 #[map]
 static CPUS: CpuMap = CpuMap::with_max_entries(1, 0);
 
+/// Hits of a probe, used to test program chaining through CpuMap/DevMap.
+/// The first slot counts how many times the "raw" xdp program got executed, while the second slot
+/// counts how many times the map programs got executed.
+/// This allows the test harness to assert that a specific step got executed.
+#[map]
+static mut HITS: Array<u32> = Array::with_max_entries(2, 0);
+
 #[xdp]
 pub fn redirect_sock(_ctx: XdpContext) -> u32 {
     SOCKS.redirect(0, xdp_action::XDP_ABORTED as u64)
@@ -24,17 +31,39 @@ pub fn redirect_sock(_ctx: XdpContext) -> u32 {
 
 #[xdp]
 pub fn redirect_dev(_ctx: XdpContext) -> u32 {
+    inc_hit(0);
     DEVS.redirect(0, xdp_action::XDP_ABORTED as u64)
 }
 
 #[xdp]
 pub fn redirect_dev_hash(_ctx: XdpContext) -> u32 {
+    inc_hit(0);
     DEVS_HASH.redirect(10, xdp_action::XDP_ABORTED as u64)
 }
 
 #[xdp]
 pub fn redirect_cpu(_ctx: XdpContext) -> u32 {
+    inc_hit(0);
     CPUS.redirect(0, xdp_action::XDP_ABORTED as u64)
+}
+
+#[xdp(map = "cpumap")]
+pub fn redirect_cpu_chain(_ctx: XdpContext) -> u32 {
+    inc_hit(1);
+    xdp_action::XDP_PASS
+}
+
+#[xdp(map = "devmap")]
+pub fn redirect_dev_chain(_ctx: XdpContext) -> u32 {
+    inc_hit(1);
+    xdp_action::XDP_PASS
+}
+
+#[inline(always)]
+fn inc_hit(index: u32) {
+    if let Some(hit) = unsafe { HITS.get_ptr_mut(index) } {
+        unsafe { *hit += 1 };
+    }
 }
 
 #[cfg(not(test))]
