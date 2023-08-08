72afd877b54b0ceb24944d573ddfe676d8adc5ae
Tamir Duberstein <tamird@gmail.com>
2023-08-08 09:25:37 -0400

    integration-test: clang-format C files

--- test/integration-test/bpf/main.bpf.c
+++ test/integration-test/bpf/main.bpf.c
@@ -1,10 +1,9 @@
+// clang-format off
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+// clang-format on
 
 SEC("xdp")
-int xdp_pass(struct xdp_md *ctx)
-{
-    return XDP_PASS;
-}
+int xdp_pass(struct xdp_md *ctx) { return XDP_PASS; }
 
 char _license[] SEC("license") = "GPL";
--- test/integration-test/bpf/multimap-btf.bpf.c
+++ test/integration-test/bpf/multimap-btf.bpf.c
@@ -1,30 +1,30 @@
+// clang-format off
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
+// clang-format on
 
 struct {
-	__uint(type, BPF_MAP_TYPE_ARRAY);
-	__type(key, __u32);
-	__type(value, __u64);
-	__uint(max_entries, 1);
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __type(key, __u32);
+  __type(value, __u64);
+  __uint(max_entries, 1);
 } map_1 SEC(".maps");
 
 struct {
-	__uint(type, BPF_MAP_TYPE_ARRAY);
-	__type(key, __u32);
-	__type(value, __u64);
-	__uint(max_entries, 1);
+  __uint(type, BPF_MAP_TYPE_ARRAY);
+  __type(key, __u32);
+  __type(value, __u64);
+  __uint(max_entries, 1);
 } map_2 SEC(".maps");
 
-
 SEC("tracepoint")
-int bpf_prog(void *ctx)
-{
-	__u32 key = 0;
-	__u64 twenty_four = 24;
-	__u64 forty_two = 42;
-    bpf_map_update_elem(&map_1, &key, &twenty_four, BPF_ANY);
-    bpf_map_update_elem(&map_2, &key, &forty_two, BPF_ANY);
-	return 0;
+int bpf_prog(void *ctx) {
+  __u32 key = 0;
+  __u64 twenty_four = 24;
+  __u64 forty_two = 42;
+  bpf_map_update_elem(&map_1, &key, &twenty_four, BPF_ANY);
+  bpf_map_update_elem(&map_2, &key, &forty_two, BPF_ANY);
+  return 0;
 }
 
 char _license[] SEC("license") = "GPL";
