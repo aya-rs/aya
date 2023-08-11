02124002c88d7a89d6c9afd89857c4c301e09801
Dave Tucker <dave@dtucker.co.uk>
2023-08-11 10:28:46 +0100

    .github: Add clang-format
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- bpf/aya-bpf-bindings/include/bindings.h
+++ bpf/aya-bpf-bindings/include/bindings.h
@@ -3,9 +3,9 @@
 // https://elixir.bootlin.com/linux/v5.13/source/include/uapi/linux/types.h
 typedef __u32 __bitwise __wsum;
 
+#include "bpf_helpers.h"
 #include <linux/bpf.h>
-#include <linux/ptrace.h>
-#include <sys/socket.h>
 // needed for TC_ACT_*
 #include <linux/pkt_cls.h>
-#include "bpf_helpers.h"
+#include <linux/ptrace.h>
+#include <sys/socket.h>
