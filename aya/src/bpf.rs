0bf97eba64b44835300d8291cd4f78c220c3ad48
astoycos <astoycos@redhat.com>
2023-09-21 13:50:21 -0400

    aya/maps: fix libbpf_pin_by_name
    Aligns with libbpf for the special LIBBPF_PIN_BY_NAME
    map flag. Specifically if the flag is provided without a pin path
    default to "/sys/fs/bpf".
    
    Signed-off-by: astoycos <astoycos@redhat.com>

--- aya/src/bpf.rs
+++ aya/src/bpf.rs
@@ -492,8 +492,13 @@ impl<'a> BpfLoader<'a> {
             let mut map = match obj.pinning() {
                 PinningType::None => MapData::create(obj, &name, btf_fd)?,
                 PinningType::ByName => {
-                    let path = map_pin_path.as_ref().ok_or(BpfError::NoPinPath)?;
-                    MapData::create_pinned(path, obj, &name, btf_fd)?
+                    // pin maps in /sys/fs/bpf by default to align with libbpf
+                    // behavior https://github.com/libbpf/libbpf/blob/v1.2.2/src/libbpf.c#L2161.
+                    let path = map_pin_path
+                        .as_deref()
+                        .unwrap_or_else(|| Path::new("/sys/fs/bpf"));
+
+                    MapData::create_pinned_by_name(path, obj, &name, btf_fd)?
                 }
             };
             map.finalize()?;
@@ -950,10 +955,6 @@ pub enum BpfError {
         /// The original io::Error
         error: io::Error,
     },
-
-    /// Pinning requested but no path provided
-    #[error("pinning requested but no path provided")]
-    NoPinPath,
 
     /// Unexpected pinning type
     #[error("unexpected pinning type {name}")]
