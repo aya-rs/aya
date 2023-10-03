c1ba517dbfa8d0033903138d88ad2306b02eb61f
Mike Rostecki <vadorovsky@protonmail.com>
2023-10-03 17:36:18 +0100

    bpf: Handle raw tracepoint arguments
    Provide an `arg()` method in `RawTracepointArgs` wrapper of
    `bpf_raw_tracepoint_args` and also in `RawTracepointContext`, so
    it's directly available in raw tracepoint programs.
    
    The methods and traits implemented here are unsafe. There is no
    way to reliably check the number of available arguments, so
    requesting a non-existing one leads to undefined behavior.

---
+++ test/integration-test/src/tests/raw_tracepoint.rs
@@ -0,0 +1,40 @@
+use aya::{maps::Array, programs::RawTracePoint, Ebpf};
+use integration_common::raw_tracepoint::SysEnterEvent;
+
+fn get_event(bpf: &mut Ebpf) -> SysEnterEvent {
+    let map: Array<_, SysEnterEvent> = Array::try_from(bpf.map_mut("RESULT").unwrap()).unwrap();
+    map.get(&0, 0).unwrap()
+}
+
+#[test]
+fn raw_tracepoint() {
+    let mut bpf = Ebpf::load(crate::RAW_TRACEPOINT).unwrap();
+
+    // Check start condition.
+    {
+        let SysEnterEvent {
+            common_type,
+            common_flags,
+            ..
+        } = get_event(&mut bpf);
+        assert_eq!(common_type, 0);
+        assert_eq!(common_flags, 0);
+    }
+
+    // NB: we cannot fetch `map` just once above because both `Ebpf::map_mut` and
+    // `Ebpf::program_mut` take &mut self, resulting in overlapping mutable borrows.
+    let prog: &mut RawTracePoint = bpf.program_mut("sys_enter").unwrap().try_into().unwrap();
+    prog.load().unwrap();
+    prog.attach("sys_enter").unwrap();
+
+    // Check that a syscall was traced.
+    {
+        let SysEnterEvent {
+            common_type,
+            common_flags,
+            ..
+        } = get_event(&mut bpf);
+        assert_ne!(common_type, 0);
+        assert_ne!(common_flags, 0);
+    }
+}
