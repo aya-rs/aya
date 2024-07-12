787d08b0317567a625173f4ff954695efe6c1a7a
martinsoees <martinsoees@proton.me>
2024-07-12 03:31:46 -0400

    remove unintended explicit type declaration in code unrelated to this PR

--- aya/src/bpf.rs
+++ aya/src/bpf.rs
@@ -537,7 +537,7 @@ impl<'a> EbpfLoader<'a> {
                 PinningType::ByName => {
                     // pin maps in /sys/fs/bpf by default to align with libbpf
                     // behavior https://github.com/libbpf/libbpf/blob/v1.2.2/src/libbpf.c#L2161.
-                    let path: &Path = map_pin_path
+                    let path = map_pin_path
                         .as_deref()
                         .unwrap_or_else(|| Path::new("/sys/fs/bpf"));
 
