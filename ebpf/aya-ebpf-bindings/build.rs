2288b0911515fd501696abb7e735b4ed2baff65a
Eric Long <i@hack3r.moe>
2025-01-21 18:16:25 +0800

    Fix aya-ebpf-* riscv64 build
    bpf_target_arch should be riscv64, but target triple starts with riscv64gc (and possibly riscv64 followed by any combination of extensions).

--- ebpf/aya-ebpf-bindings/build.rs
+++ ebpf/aya-ebpf-bindings/build.rs
@@ -6,7 +6,10 @@ fn main() {
         println!("cargo:rustc-cfg=bpf_target_arch=\"{arch}\"");
     } else {
         let arch = env::var("HOST").unwrap();
-        let arch = arch.split_once('-').map_or(&*arch, |x| x.0);
+        let mut arch = arch.split_once('-').map_or(&*arch, |x| x.0);
+        if arch.starts_with("riscv64") {
+            arch = "riscv64";
+        }
         println!("cargo:rustc-cfg=bpf_target_arch=\"{arch}\"");
     }
     println!("cargo::rustc-check-cfg=cfg(bpf_target_arch, values(\"x86_64\",\"arm\",\"aarch64\",\"riscv64\",\"powerpc64\",\"s390x\"))");
