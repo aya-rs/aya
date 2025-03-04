ebad9a3dcc22c0d42856c300d1f89cb831c052cc
Tamir Duberstein <tamird@gmail.com>
2025-03-04 12:59:14 -0500

    codegen: tidy up
    - Document the need for external rustfmt invocation.
    - Remove reexports.
    - Remove `write_to_file`.
    - Avoid allocating strings when using bindgen to write bindings.

--- aya-tool/src/lib.rs
+++ aya-tool/src/lib.rs
@@ -1,22 +1,2 @@
-use std::{
-    fs::{File, create_dir_all},
-    io::{self, Write},
-    path::Path,
-};
-
 pub mod bindgen;
 pub mod generate;
-
-pub use generate::{InputFile, generate};
-
-pub fn write_to_file<T: AsRef<Path>>(path: T, code: &str) -> Result<(), io::Error> {
-    // Create parent directories if they don't exist already
-    if let Some(parent) = path.as_ref().parent() {
-        if !parent.exists() {
-            create_dir_all(parent)?;
-        }
-    }
-
-    let mut file = File::create(path)?;
-    file.write_all(code.as_bytes())
-}
