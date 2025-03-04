3f4c7fa1e52b80b7e687580e145ec15c40ce72b3
Tamir Duberstein <tamird@gmail.com>
2025-03-04 12:08:09 -0500

    aya-obj,aya-ebpf-*: hook up loongarch64
    This causes rustfmt to format those files.
    
    Squish some other conditional compilation to get rustfmt sorting.

--- ebpf/aya-ebpf/src/args.rs
+++ ebpf/aya-ebpf/src/args.rs
@@ -1,14 +1,16 @@
 #[cfg(any(
-    bpf_target_arch = "x86_64",
     bpf_target_arch = "arm",
-    bpf_target_arch = "powerpc64",
     bpf_target_arch = "mips",
+    bpf_target_arch = "powerpc64",
+    bpf_target_arch = "x86_64",
 ))]
 use crate::bindings::pt_regs;
-// aarch64 uses user_pt_regs instead of pt_regs
-#[cfg(any(bpf_target_arch = "aarch64", bpf_target_arch = "s390x"))]
+#[cfg(any(
+    bpf_target_arch = "aarch64",
+    bpf_target_arch = "loongarch64",
+    bpf_target_arch = "s390x",
+))]
 use crate::bindings::user_pt_regs as pt_regs;
-// riscv64 uses user_regs_struct instead of pt_regs
 #[cfg(bpf_target_arch = "riscv64")]
 use crate::bindings::user_regs_struct as pt_regs;
 use crate::{bindings::bpf_raw_tracepoint_args, cty::c_void, helpers::bpf_probe_read};
