e283ee20022aa1a51bb6754c56115b7dbc5d484b
Michal Rostecki <vadorovsky@protonmail.com>
2025-03-12 09:11:53 +0100

    aya: Ensure that truncated map names are NUL terminated
    Limit of map names in eBPF is 16 bytes and they have to be NUL
    terminated.
    
    Before this change, long names were truncated to 16 bytes.
    `MAP_WITH_LOOOONG_NAAAAAAAAME` would become `MAP_WITH_LOOOONG`, which
    doesn't contain the NUL byte.
    
    This change fixes that by truncating the name to 15 bytes, ensuring
    that the 16th byte is NUL. `MAP_WITH_LOOOONG_NAAAAAAAAME` is truncated
    to `MAP_WITH_LOOOON\0`.

--- aya/src/sys/bpf.rs
+++ aya/src/sys/bpf.rs
@@ -93,9 +93,9 @@ pub(crate) fn bpf_create_map(
     // The map name was added as a parameter in kernel 4.15+ so we skip adding it on
     // older kernels for compatibility
     if KernelVersion::at_least(4, 15, 0) {
+        let name_bytes = name.to_bytes();
         // u.map_name is 16 bytes max and must be NULL terminated
-        let name_bytes = name.to_bytes_with_nul();
-        let len = cmp::min(name_bytes.len(), u.map_name.len());
+        let len = cmp::min(name_bytes.len(), u.map_name.len() - 1);
         u.map_name[..len]
             .copy_from_slice(unsafe { mem::transmute::<&[u8], &[c_char]>(&name_bytes[..len]) });
     }
