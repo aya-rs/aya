e18d57acd70e7ae72cbb682323205ab00687c723
altug bozkurt <altug.bozkurt09@gmail.com>
2025-10-22 15:40:26 +0300

    aya: Support loading programs with ksyms

---
+++ test/integration-test/bpf/ksyms_typeless_missing.bpf.c
@@ -0,0 +1,19 @@
+// clang-format off
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+// clang-format on
+
+#ifndef __ksym
+#define __ksym __attribute__((section(".ksyms")))
+#endif
+
+char _license[] SEC("license") = "GPL";
+
+extern const void totally_bogus_typeless_symbol __ksym;
+
+SEC("tp_btf/sys_enter")
+int BPF_PROG(ksyms_typeless_missing, struct pt_regs *regs, long id) {
+  return (int)(unsigned long)&totally_bogus_typeless_symbol;
+}
