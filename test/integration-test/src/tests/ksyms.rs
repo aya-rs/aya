6eed2b9b54c9d7863ba11668ba2220f1437a9ae7
altug bozkurt <altug.bozkurt09@gmail.com>
2025-10-22 15:40:26 +0300

    aya: Support loading programs with ksyms

---
+++ test/integration-test/src/tests/ksyms.rs
@@ -0,0 +1,11 @@
+use aya::{Btf, Ebpf, programs::BtfTracePoint};
+use test_log::test;
+
+#[test]
+fn test_ksyms() {
+    let mut ebpf = Ebpf::load(crate::KSYMS).unwrap();
+    let prog: &mut BtfTracePoint = ebpf.program_mut("sys_enter").unwrap().try_into().unwrap();
+    let btf = Btf::from_sys_fs().unwrap();
+    prog.load("sys_enter", &btf).unwrap();
+    prog.attach().unwrap();
+}
