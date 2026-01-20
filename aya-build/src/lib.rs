388d1f9694f5eb49f1030caf9a17e71679e153e9
vad <vad.sol@proton.me>
2026-01-20 13:50:15 +0100

    aya-build: Allow to opt out
    Allow to opt out from cargo-in-cargo by setting `AYA_BUILD_SKIP`
    environment variable to `1` or `true`. That makes it easier for people
    using custom toolchains not managed by rustup (e.g. package
    maintainers).
    
    Fixes: #1329

--- aya-build/src/lib.rs
+++ aya-build/src/lib.rs
@@ -43,6 +43,18 @@ pub fn build_ebpf<'a>(
     packages: impl IntoIterator<Item = Package<'a>>,
     toolchain: Toolchain<'a>,
 ) -> Result<()> {
+    const AYA_BUILD_SKIP: &str = "AYA_BUILD_SKIP";
+    println!("cargo:rerun-if-env-changed={AYA_BUILD_SKIP}");
+    if let Some(aya_build_skip) = env::var_os(AYA_BUILD_SKIP)
+        && (aya_build_skip.eq("1") || aya_build_skip.eq_ignore_ascii_case("true"))
+    {
+        println!(
+            "cargo:warning={AYA_BUILD_SKIP}={}; skipping eBPF build",
+            aya_build_skip.display()
+        );
+        return Ok(());
+    }
+
     let out_dir = env::var_os("OUT_DIR").ok_or(anyhow!("OUT_DIR not set"))?;
     let out_dir = PathBuf::from(out_dir);
 
