0cff23ceaa452568b41ff68598dd0ad47d0f6ce8
root <contact@brskt.be>
2026-01-23 14:28:30 +0100

    refactor(aya-ebpf): modify btf_map_def! to generate flat structs
    Modify the btf_map_def! macro to generate flat #[repr(C)] structs
    instead of UnsafeCell wrappers. This produces BTF that both aya
    and libbpf can parse.
    
    Also remove UnsafeCell traversal code from aya-obj loader since
    it is no longer needed with flat struct layout.

--- ebpf/aya-ebpf/src/btf_maps/mod.rs
+++ ebpf/aya-ebpf/src/btf_maps/mod.rs
@@ -19,42 +19,105 @@ impl AyaBtfMapMarker {
     }
 }
 
-#[macro_export]
+/// Defines a BTF-compatible map struct with flat `#[repr(C)]` layout.
+///
+/// This macro generates a map definition struct that produces BTF metadata
+/// compatible with both aya and libbpf loaders.
 macro_rules! btf_map_def {
-    ($name:ident, $t:ident $(, $field:ident : $ty:ty)* $(,)?) => {
-        #[expect(
-            dead_code,
-            reason = "These fields exist only for BTF metadata exposure. None of them are actually used."
-        )]
-        pub struct $name<K, V, const M: usize, const F: usize = 0> {
-            r#type: *const [i32; $t as usize],
-            key: *const K,
-            value: *const V,
+    // Variant for maps with <T, const M: usize, const F: usize> generics
+    (
+        $(#[$attr:meta])*
+        $vis:vis struct $name:ident<T, const M: usize, const F: usize>,
+        map_type: $map_type:ident,
+        key: $key_ty:ty
+        $(, $extra_field:ident : $extra_ty:ty)*
+        $(,)?
+    ) => {
+        $(#[$attr])*
+        #[repr(C)]
+        #[allow(dead_code)]
+        $vis struct $name<T, const M: usize, const F: usize = 0> {
+            r#type: *const [i32; $crate::bindings::bpf_map_type::$map_type as usize],
+            key: *const $key_ty,
+            value: *const T,
             max_entries: *const [i32; M],
             map_flags: *const [i32; F],
-
-            $($field: $ty,)*
-
-            // Anonymize the struct.
+            $($extra_field: $extra_ty,)*
             _anon: $crate::btf_maps::AyaBtfMapMarker,
         }
 
-        #[expect(
-            clippy::new_without_default,
-            reason = "BPF maps are always used as static variables, therefore this method has to be `const`. `Default::default` is not `const`."
-        )]
-        impl<K, V, const M: usize, const F: usize> $name<K, V, M, F> {
-            pub const fn new() -> $name<K, V, M, F> {
+        unsafe impl<T: Sync, const M: usize, const F: usize> Sync for $name<T, M, F> {}
+
+        impl<T, const M: usize, const F: usize> $name<T, M, F> {
+            #[expect(
+                clippy::new_without_default,
+                reason = "BPF maps are always used as static variables, therefore this method has to be `const`. `Default::default` is not `const`."
+            )]
+            pub const fn new() -> Self {
                 Self {
                     r#type: ::core::ptr::null(),
                     key: ::core::ptr::null(),
                     value: ::core::ptr::null(),
-                    $($field: ::core::ptr::null(),)*
+                    max_entries: ::core::ptr::null(),
+                    map_flags: ::core::ptr::null(),
+                    $($extra_field: ::core::ptr::null(),)*
+                    _anon: $crate::btf_maps::AyaBtfMapMarker::new(),
+                }
+            }
+
+            #[inline(always)]
+            pub(crate) fn as_ptr(&self) -> *mut ::core::ffi::c_void {
+                ::core::ptr::from_ref(self).cast_mut().cast()
+            }
+        }
+    };
+
+    // Variant for maps with <T> only and fixed max_entries/map_flags
+    (
+        $(#[$attr:meta])*
+        $vis:vis struct $name:ident<T>,
+        map_type: $map_type:ident,
+        key: $key_ty:ty,
+        max_entries: $max_entries:expr,
+        map_flags: $map_flags:expr
+        $(,)?
+    ) => {
+        $(#[$attr])*
+        #[repr(C)]
+        #[allow(dead_code)]
+        $vis struct $name<T> {
+            r#type: *const [i32; $crate::bindings::bpf_map_type::$map_type as usize],
+            key: *const $key_ty,
+            value: *const T,
+            max_entries: *const [i32; $max_entries],
+            map_flags: *const [i32; $map_flags],
+            _anon: $crate::btf_maps::AyaBtfMapMarker,
+        }
+
+        unsafe impl<T: Sync> Sync for $name<T> {}
+
+        impl<T> $name<T> {
+            #[expect(
+                clippy::new_without_default,
+                reason = "BPF maps are always used as static variables, therefore this method has to be `const`. `Default::default` is not `const`."
+            )]
+            pub const fn new() -> Self {
+                Self {
+                    r#type: ::core::ptr::null(),
+                    key: ::core::ptr::null(),
+                    value: ::core::ptr::null(),
                     max_entries: ::core::ptr::null(),
                     map_flags: ::core::ptr::null(),
                     _anon: $crate::btf_maps::AyaBtfMapMarker::new(),
                 }
             }
+
+            #[inline(always)]
+            pub(crate) fn as_ptr(&self) -> *mut ::core::ffi::c_void {
+                ::core::ptr::from_ref(self).cast_mut().cast()
+            }
         }
     };
 }
+
+pub(crate) use btf_map_def;
