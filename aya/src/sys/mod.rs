8465c2645713460788d46d604b84c03d609b774c
hsqStephenZhang <stephenzhang666666@gmail.com>
2026-01-26 18:51:07 +0100

    chore: fmt

--- aya/src/sys/mod.rs
+++ aya/src/sys/mod.rs
@@ -16,9 +16,9 @@ use std::{
 
 use aya_obj::generated::{bpf_attr, bpf_cmd, bpf_stats_type, perf_event_attr};
 pub(crate) use bpf::*;
+pub use bpf::{TestRunOptions, TestRunResult};
 #[cfg(test)]
 pub(crate) use fake::*;
-pub use bpf::{TestRunOptions, TestRunResult};
 pub use feature_probe::{is_map_supported, is_program_supported};
 #[doc(hidden)]
 pub use netlink::netlink_set_link_up;
