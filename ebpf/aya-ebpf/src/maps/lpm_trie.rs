a17985e4bbddfe92766f59a361ffd7818aee8eb8
Brskt <contact@brskt.be>
2026-01-29 18:45:56 +0100

    aya-ebpf: add HashOfMaps and ArrayOfMaps
    This adds support for BPF_MAP_TYPE_ARRAY_OF_MAPS and
    BPF_MAP_TYPE_HASH_OF_MAPS on the eBPF side.
    
    Implements:
    - ArrayOfMaps and HashOfMaps for legacy maps
    - ArrayOfMaps and HashOfMaps for BTF maps
    - InnerMap sealed trait to mark types usable as inner maps

--- ebpf/aya-ebpf/src/maps/lpm_trie.rs
+++ ebpf/aya-ebpf/src/maps/lpm_trie.rs
@@ -15,6 +15,8 @@ pub struct LpmTrie<K, V> {
     _kv: PhantomData<(K, V)>,
 }
 
+impl<K, V> super::private::Map for LpmTrie<K, V> {}
+
 #[repr(C, packed)]
 pub struct Key<K> {
     /// Represents the number of bits matched against.
