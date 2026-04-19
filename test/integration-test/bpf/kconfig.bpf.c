532c3e10229ac0c9f5f40999bb9e60f891dfd7c2
Davide Bertola <dade@dadeb.it>
2026-04-19 20:15:09 +0200

    integration-test: fix kconfig coverage across kernels

--- test/integration-test/bpf/kconfig.bpf.c
+++ test/integration-test/bpf/kconfig.bpf.c
@@ -16,19 +16,19 @@
 struct {
   __uint(type, BPF_MAP_TYPE_ARRAY);
   __type(key, __u32);
-  __type(value, __u64);
+  __type(value, __s64);
   __uint(max_entries, SECOND_STRING_INDEX + SECOND_STRING_LEN);
 } RESULTS SEC(".maps");
 
 extern _Bool CONFIG_BPF __kconfig;
-extern unsigned short CONFIG_PANIC_TIMEOUT __kconfig;
-extern unsigned int CONFIG_DEFAULT_HUNG_TASK_TIMEOUT __kconfig;
+extern int CONFIG_PANIC_TIMEOUT __kconfig;
+extern unsigned long CONFIG_DEFAULT_HUNG_TASK_TIMEOUT __kconfig __weak;
 extern _Bool CONFIG_BPF_JIT __kconfig;
 extern char CONFIG_DEFAULT_HOSTNAME[DEFAULT_HOSTNAME_LEN] __kconfig;
-extern char CONFIG_FIRST_STRING[] __kconfig;
-extern char CONFIG_SECOND_STRING[] __kconfig;
+extern char CONFIG_FIRST_STRING[] __kconfig __weak;
+extern char CONFIG_SECOND_STRING[] __kconfig __weak;
 
-static long set_result(__u32 key, __u64 value) {
+static long set_result(__u32 key, __s64 value) {
   return bpf_map_update_elem(&RESULTS, &key, &value, BPF_ANY);
 }
 
@@ -49,7 +49,7 @@ int test_kconfig(void *ctx) {
   }
   for (int i = 0; i < DEFAULT_HOSTNAME_LEN; i++) {
     if (set_result(DEFAULT_HOSTNAME_INDEX + i,
-                   (__u64)(__u8)CONFIG_DEFAULT_HOSTNAME[i]) != 0) {
+                   (__s64)(__u8)CONFIG_DEFAULT_HOSTNAME[i]) != 0) {
       return 1;
     }
   }
@@ -60,13 +60,13 @@ SEC("uprobe")
 int test_kconfig_unsized_strings(void *ctx) {
   for (int i = 0; i < FIRST_STRING_LEN; i++) {
     if (set_result(FIRST_STRING_INDEX + i,
-                   (__u64)(__u8)CONFIG_FIRST_STRING[i]) != 0) {
+                   (__s64)(__u8)CONFIG_FIRST_STRING[i]) != 0) {
       return 1;
     }
   }
   for (int i = 0; i < SECOND_STRING_LEN; i++) {
     if (set_result(SECOND_STRING_INDEX + i,
-                   (__u64)(__u8)CONFIG_SECOND_STRING[i]) != 0) {
+                   (__s64)(__u8)CONFIG_SECOND_STRING[i]) != 0) {
       return 1;
     }
   }
