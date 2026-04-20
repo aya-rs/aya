800a932fdc46ae98d70676ebfd36915ce9c72b50
Davide Bertola <dade@dadeb.it>
2026-04-20 19:27:06 +0200

    aya: reformat tests

--- test/integration-test/bpf/kconfig_missing_strong.bpf.c
+++ test/integration-test/bpf/kconfig_missing_strong.bpf.c
@@ -6,8 +6,6 @@
 extern unsigned int CONFIG_REQUIRED __kconfig;
 
 SEC("uprobe")
-int test_kconfig_missing_strong(void *ctx) {
-  return CONFIG_REQUIRED != 0;
-}
+int test_kconfig_missing_strong(void *ctx) { return CONFIG_REQUIRED != 0; }
 
 char _license[] SEC("license") = "GPL";
