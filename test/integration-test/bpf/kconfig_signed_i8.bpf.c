24dc44ed1169feec286298484098860b2af4186d
Davide Bertola <dade@dadeb.it>
2026-04-20 10:54:52 +0200

    integration-test: move kconfig coverage end to end

---
+++ test/integration-test/bpf/kconfig_signed_i8.bpf.c
@@ -0,0 +1,13 @@
+// clang-format off
+#include <vmlinux.h>
+#include <bpf/bpf_helpers.h>
+// clang-format on
+
+extern signed char CONFIG_SIGNED_VALUE __kconfig;
+
+SEC("uprobe")
+int test_kconfig_signed_i8(void *ctx) {
+  return CONFIG_SIGNED_VALUE != 0;
+}
+
+char _license[] SEC("license") = "GPL";
