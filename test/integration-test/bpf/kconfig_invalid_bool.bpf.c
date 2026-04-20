e8286f5d3d7801e2146906fac4e3f1be1dc3b80e
Davide Bertola <dade@dadeb.it>
2026-04-20 19:27:06 +0200

    aya: reformat tests

--- test/integration-test/bpf/kconfig_invalid_bool.bpf.c
+++ test/integration-test/bpf/kconfig_invalid_bool.bpf.c
@@ -6,8 +6,6 @@
 extern _Bool CONFIG_BOOL_VALUE __kconfig;
 
 SEC("uprobe")
-int test_kconfig_invalid_bool(void *ctx) {
-  return CONFIG_BOOL_VALUE;
-}
+int test_kconfig_invalid_bool(void *ctx) { return CONFIG_BOOL_VALUE; }
 
 char _license[] SEC("license") = "GPL";
