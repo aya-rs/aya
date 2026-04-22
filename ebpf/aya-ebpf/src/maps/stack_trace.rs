937f1167eca5b1f2b5a61cd84c12648bb44019f2
Brskt <contact@brskt.be>
2026-04-23 00:20:19 +0200

    aya-ebpf, integration-ebpf: migrate to StackIdContext::get_stackid
    Delete the `pub unsafe fn get_stackid<C: EbpfContext>` methods from
    both the legacy and BTF `StackTrace`. Migrate the integration-test
    macro to `ctx.get_stackid(&$map, flags)`.

--- ebpf/aya-ebpf/src/maps/stack_trace.rs
+++ ebpf/aya-ebpf/src/maps/stack_trace.rs
@@ -1,12 +1,7 @@
-use core::borrow::Borrow;
-
 use aya_ebpf_bindings::bindings::PERF_MAX_STACK_DEPTH;
 
 use crate::{
-    EbpfContext,
     bindings::bpf_map_type::BPF_MAP_TYPE_STACK_TRACE,
-    cty::c_long,
-    helpers::bpf_get_stackid,
     maps::{MapDef, PinningType},
 };
 
@@ -21,20 +16,6 @@ impl StackTrace {
         [u64; PERF_MAX_STACK_DEPTH as usize],
         BPF_MAP_TYPE_STACK_TRACE
     );
-
-    #[expect(
-        clippy::missing_safety_doc,
-        reason = "safety requirements come from the underlying helper"
-    )]
-    pub unsafe fn get_stackid<C: EbpfContext>(
-        &self,
-        ctx: impl Borrow<C>,
-        flags: u64,
-    ) -> Result<c_long, i32> {
-        let ret =
-            unsafe { bpf_get_stackid(ctx.borrow().as_ptr(), self.def.as_ptr().cast(), flags) };
-        if ret < 0 { Err(ret as i32) } else { Ok(ret) }
-    }
 }
 
 impl crate::programs::tracing::private::StackTraceMap for StackTrace {
