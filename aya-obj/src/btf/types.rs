0340af9a26a5a8736fadbe6891b6d5eb14c77193
Davide Bertola <dade@dadeb.it>
2026-06-03 11:41:28 +0200

    aya-obj: use BPF pointer size for object BTF

--- aya-obj/src/btf/types.rs
+++ aya-obj/src/btf/types.rs
@@ -1322,13 +1322,8 @@ impl BtfType {
             Self::Struct(t) => Some(t.size),
             Self::Union(t) => Some(t.size),
             Self::DataSec(t) => Some(t.size),
-            Self::Ptr(_) => Some(Self::ptr_size()),
             _ => None,
         }
-    }
-
-    pub(crate) const fn ptr_size() -> u32 {
-        size_of::<&()>() as u32
     }
 
     pub(crate) const fn btf_type(&self) -> Option<u32> {
