f3dea00341c597c17ecb40017ad3da4f6b0c1c96
viboognesh <gviboognesh99@gmail.com>
2026-08-26 21:47:51 +0530

    aya: return error for cstring conversions instead of unwrap
    Signed-off-by: viboognesh <gviboognesh99@gmail.com>

--- aya/src/programs/tc.rs
+++ aya/src/programs/tc.rs
@@ -372,8 +372,10 @@ impl SchedClassifier {
         match options {
             TcAttachOptions::Netlink(options) => {
                 let name = self.data.name.as_deref().unwrap_or_default();
-                // TODO: avoid this unwrap by adding a new error variant.
-                let name = CString::new(name).unwrap();
+                let name = CString::new(name).map_err(|error| ProgramError::InvalidName {
+                    name: name.to_owned(),
+                    error,
+                })?;
                 let (priority, handle) = unsafe {
                     netlink_qdisc_attach(
                         if_index as i32,
